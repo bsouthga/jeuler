7bfdeb0e8dd7acafff76c0c1dbfabf510a7edd18
Ben Southgate <bsouthga@gmail.com>
2014-04-10 23:41:00 -0400

    woo problem 2

--- problems/p1.jl
+++ problems/p1.jl
@@ -1,4 +1,4 @@
 # Project Euler Problem 1
 # 4-10-14, Ben Southgate
 
-println(sum([(x%3==0 || x%5==0 ? x : 0) for x=1:999]))
+println(sum(@task for x=1:999; produce(x%3==0 || x%5==0 ? x : 0); end))
