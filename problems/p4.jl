f224d51b4913b4e8310bfe0cddd9994619e454b2
Ben Southgate <bsouthga@gmail.com>
2014-04-13 19:00:28 -0400

    spacing

--- problems/p4.jl
+++ problems/p4.jl
@@ -5,17 +5,17 @@
 # Find the largest palindrome made from the product of two 3-digit numbers.
 
 function palindrome()
-	largest::Int64 = 0
-	for i=999:-1:100
-		for j=999:-1:100
-			n::Int64 = i*j
-			s = "$(n)"
-			if s == reverse(s)
-				largest = largest < n ? n : largest
-			end
-		end
-	end
-	return largest
+  largest::Int64 = 0
+  for i=999:-1:100
+    for j=999:-1:100
+      n::Int64 = i*j
+      s = "$(n)"
+      if s == reverse(s)
+        largest = largest < n ? n : largest
+      end
+    end
+  end
+  return largest
 end
 
 print(palindrome())
