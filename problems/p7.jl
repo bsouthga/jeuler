f9723b3955ec6a21110c0eedfafb5d8c705c237b
Ben Southgate <bsouthga@gmail.com>
2014-04-20 14:11:55 -0400

    spacing

--- problems/p7.jl
+++ problems/p7.jl
@@ -7,18 +7,19 @@
 
 prime_list = [2]
 curr_number = 3
+
 while (length(prime_list) < 10001)
-	number_is_prime = true
-	for num in prime_list
-		if curr_number % num == 0
-			number_is_prime = false
-			break
-		end
-	end
-	if number_is_prime
-		prime_list = push!(prime_list, curr_number)
-	end
-	curr_number += 1
+  number_is_prime = true
+  for num in prime_list
+    if curr_number % num == 0
+      number_is_prime = false
+      break
+    end
+  end
+  if number_is_prime
+    prime_list = push!(prime_list, curr_number)
+  end
+  curr_number += 1
 end
 
-println(prime_list[end])
+println(prime_list[end]) 
